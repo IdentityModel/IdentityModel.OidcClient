713461748ac837a71524424be86b50a52dfa235f
Dominick Baier <dbaier@leastprivilege.com>
2016-12-18 22:50:07 +0100

    start of new OM

---
+++ src/IdentityModel.OidcClient/Options.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityModel.OidcClient
+{
+    public class Options
+    {
+        public string Authority { get; set; }
+
+    }
+}
---
+++ src/IdentityModel.OidcClient/Policy.cs
@@ -0,0 +1,11 @@
+﻿namespace IdentityModel.OidcClient
+{
+    public class Policy
+    {
+        public bool ValidateIssuerName { get; set; } = true;
+        public bool ValidateEndpoints { get; set; } = true;
+
+        public bool RequireCodeHash { get; set; } = true;
+        public bool RequireAccessTokenHash { get; set; } = true;
+    }
+}
