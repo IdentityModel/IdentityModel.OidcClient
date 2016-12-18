5078d7d2c19ac5099477e3c9629b163b1b310ab0
Dominick Baier <dbaier@leastprivilege.com>
2016-12-18 23:02:37 +0100

    more OM work

--- src/IdentityModel.OidcClient/Options.cs
+++ src/IdentityModel.OidcClient/Options.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using IdentityModel.OidcClient.WebView;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace IdentityModel.OidcClient
 {
@@ -9,5 +10,42 @@ namespace IdentityModel.OidcClient
     {
         public string Authority { get; set; }
 
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string Scope { get; }
+        public string RedirectUri { get; }
+
+        public IWebView WebView { get; }
+        public IIdentityTokenValidator IdentityTokenValidator { get; }
+
+        public bool UseFormPost { get; set; } = false;
+        public bool LoadProfile { get; set; } = true;
+        public bool FilterClaims { get; set; } = true;
+        public AuthenticationStyle Style = AuthenticationStyle.Hybrid;
+
+        public HttpMessageHandler BackchannelHandler { get; set; }
+        public TimeSpan BackchannelTimeout { get; set; } = TimeSpan.FromSeconds(30);
+
+        public Policy Policy { get; set; } = new Policy();
+        public ILoggerFactory LoggerFactory { get; } = new LoggerFactory();
+
+        public ICollection<string> FilteredClaims { get; set; } = new HashSet<string>
+        {
+            JwtClaimTypes.Issuer,
+            JwtClaimTypes.Expiration,
+            JwtClaimTypes.NotBefore,
+            JwtClaimTypes.Audience,
+            JwtClaimTypes.Nonce,
+            JwtClaimTypes.IssuedAt,
+            JwtClaimTypes.AuthenticationTime,
+            JwtClaimTypes.AuthorizationCodeHash,
+            JwtClaimTypes.AccessTokenHash
+        };
+
+        public enum AuthenticationStyle
+        {
+            AuthorizationCode,
+            Hybrid
+        }
     }
 }
--- src/IdentityModel.OidcClient/Policy.cs
+++ src/IdentityModel.OidcClient/Policy.cs
@@ -1,4 +1,7 @@
-﻿namespace IdentityModel.OidcClient
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IdentityModel.OidcClient
 {
     public class Policy
     {
@@ -7,5 +10,14 @@
 
         public bool RequireCodeHash { get; set; } = true;
         public bool RequireAccessTokenHash { get; set; } = true;
+
+        public bool RequireIdentityTokenOnRefreshTokenResponse { get; set; } = true;
+
+        public ICollection<string> SupportedAlgorithms { get; set; } = new HashSet<string>
+        {
+            OidcConstants.Algorithms.Asymmetric.RS256,
+            OidcConstants.Algorithms.Asymmetric.RS384,
+            OidcConstants.Algorithms.Asymmetric.RS512
+        };
     }
 }
