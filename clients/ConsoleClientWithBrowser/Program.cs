7526eadce244c66c9c835b10f18ea7630aa80566
Dominick Baier <dbaier@leastprivilege.com>
2017-01-10 13:49:16 +0100

    add refresh token feature to client

--- clients/ConsoleClientWithBrowser/Program.cs
+++ clients/ConsoleClientWithBrowser/Program.cs
@@ -1,6 +1,8 @@
 ﻿using IdentityModel.OidcClient;
+using Newtonsoft.Json.Linq;
 using Serilog;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ConsoleClientWithBrowser
@@ -9,6 +11,9 @@ namespace ConsoleClientWithBrowser
     {
         static string _authority = "https://demo.identityserver.io";
         static string _api = "https://api.identityserver.io/identity";
+
+        static OidcClient _oidcClient;
+        static HttpClient _apiClient = new HttpClient { BaseAddress = new Uri(_api) };
 
         public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
 
@@ -21,12 +26,10 @@ namespace ConsoleClientWithBrowser
             Console.WriteLine("Press any key to sign in...");
             Console.ReadKey();
 
-            await SignInAsync();
-
-            Console.ReadKey();
+            await SignIn();
         }
 
-        private static async Task SignInAsync()
+        private static async Task SignIn()
         {
             // create a redirect URI using an available port on the loopback address.
             string redirectUri = string.Format("http://127.0.0.1:7890/");
@@ -37,24 +40,22 @@ namespace ConsoleClientWithBrowser
                 ClientId = "native.hybrid",
                 RedirectUri = redirectUri,
                 Scope = "openid profile api",
-                FilterClaims = true,
-                LoadProfile = true,
-                Flow = OidcClientOptions.AuthenticationFlow.Hybrid,
                 Browser = new SystemBrowser(port: 7890)
             };
 
             var serilog = new LoggerConfiguration()
-                .MinimumLevel.Verbose()
+                .MinimumLevel.Debug()
                 .Enrich.FromLogContext()
                 .WriteTo.LiterateConsole(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message}{NewLine}{Exception}{NewLine}")
                 .CreateLogger();
 
             options.LoggerFactory.AddSerilog(serilog);
 
-            var client = new OidcClient(options);
-            var result = await client.LoginAsync();
+            _oidcClient = new OidcClient(options);
+            var result = await _oidcClient.LoginAsync();
 
             ShowResult(result);
+            await NextSteps(result);
         }
 
         private static void ShowResult(LoginResult result)
@@ -74,6 +75,62 @@ namespace ConsoleClientWithBrowser
             Console.WriteLine($"\nidentity token: {result.IdentityToken}");
             Console.WriteLine($"access token:   {result.AccessToken}");
             Console.WriteLine($"refresh token:  {result?.RefreshToken ?? "none"}");
+        }
+
+        private static async Task NextSteps(LoginResult result)
+        {
+            var currentAccessToken = result.AccessToken;
+            var currentRefreshToken = result.RefreshToken;
+
+            var menu = "  x...exit  c...call api   ";
+            if (currentRefreshToken != null) menu += "r...refresh token   ";
+
+            while (true)
+            {
+                Console.WriteLine("\n\n");
+
+                Console.Write(menu);
+                var key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.X) return;
+
+                if (key.Key == ConsoleKey.C) await CallApi(currentAccessToken);
+
+                if (key.Key == ConsoleKey.R)
+                {
+                    var refreshResult = await _oidcClient.RefreshTokenAsync(currentRefreshToken);
+                    if (result.IsError)
+                    {
+                        Console.WriteLine($"Error: {refreshResult.Error}");
+                    }
+                    else
+                    {
+                        currentRefreshToken = refreshResult.RefreshToken;
+                        currentAccessToken = refreshResult.AccessToken;
+
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine($"access token:   {result.AccessToken}");
+                        Console.WriteLine($"refresh token:  {result?.RefreshToken ?? "none"}");
+                    }
+                }
+            }
+        }
+
+        private static async Task CallApi(string currentAccessToken)
+        {
+            _apiClient.SetBearerToken(currentAccessToken);
+            var response = await _apiClient.GetAsync("");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = JArray.Parse(await response.Content.ReadAsStringAsync());
+                Console.WriteLine("\n\n");
+                Console.WriteLine(json);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.ReasonPhrase}");
+            }
         }
     }
 }
