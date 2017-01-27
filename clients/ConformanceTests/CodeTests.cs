203aa5e07c1c6d289bd1baa63552b928651887df
Dominick Baier <dbaier@leastprivilege.com>
2017-01-27 12:25:38 +0100

    add more conformance tests

--- clients/ConformanceTests/CodeTests.cs
+++ clients/ConformanceTests/CodeTests.cs
@@ -17,7 +17,10 @@ namespace ConformanceTests
             //await rp_response_type_code();
             //await rp_scope_userinfo_claims();
             //await rp_nonce_invalid();
-            await rp_token_endpoint_client_secret_basic();
+            //await rp_token_endpoint_client_secret_basic();
+            //await rp_id_token_aud();
+            //await rp_id_token_kid_absent_single_jwks();
+            await rp_id_token_sig_none();
         }
 
         // Make an authentication request using the Authorization Code Flow.
@@ -73,6 +76,55 @@ namespace ConformanceTests
 
             options.Scope = "openid";
             options.Flow = OidcClientOptions.AuthenticationFlow.AuthorizationCode;
+
+            var client = new OidcClient(options);
+            var result = await client.LoginAsync();
+
+            helper.ShowResult(result);
+        }
+
+        // Request an ID token and compare its aud value to the Relying Party's 'client_id'.
+        // Identify that the 'aud' value is missing or doesn't match the 'client_id' and reject the ID Token after doing ID Token validation.
+        public async Task rp_id_token_aud()
+        {
+            var helper = new Helper(_rpId, "rp-id_token-aud");
+            var options = await helper.Register();
+
+            options.Scope = "openid";
+            options.Flow = OidcClientOptions.AuthenticationFlow.AuthorizationCode;
+
+            var client = new OidcClient(options);
+            var result = await client.LoginAsync();
+
+            helper.ShowResult(result);
+        }
+
+        // Request an ID token and verify its signature using the keys provided by the Issuer.
+        // Use the single key published by the Issuer to verify the ID Tokens signature and accept the ID Token after doing ID Token validation.
+        public async Task rp_id_token_kid_absent_single_jwks()
+        {
+            var helper = new Helper(_rpId, "rp-id_token-kid-absent-single-jwks");
+            var options = await helper.Register();
+
+            options.Scope = "openid";
+            options.Flow = OidcClientOptions.AuthenticationFlow.AuthorizationCode;
+
+            var client = new OidcClient(options);
+            var result = await client.LoginAsync();
+
+            helper.ShowResult(result);
+        }
+
+        // Use Code Flow and retrieve an unsigned ID Token.
+        // Accept the ID Token after doing ID Token validation.
+        public async Task rp_id_token_sig_none()
+        {
+            var helper = new Helper(_rpId, "rp-id_token-sig-none");
+            var options = await helper.Register();
+
+            options.Scope = "openid";
+            options.Flow = OidcClientOptions.AuthenticationFlow.AuthorizationCode;
+            options.Policy.RequireIdentityTokenSignature = false;
 
             var client = new OidcClient(options);
             var result = await client.LoginAsync();
