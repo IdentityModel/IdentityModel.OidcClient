7e08426b74f896f61e270ca7a5bcc3400c4631f8
Dominick Baier <dbaier@leastprivilege.com>
2021-02-12 12:52:07 +0100

    update test client

--- clients/ConsoleClientWithBrowser/SystemBrowser.cs
+++ clients/ConsoleClientWithBrowser/SystemBrowser.cs
@@ -141,21 +141,6 @@ namespace ConsoleClientWithBrowser
                 {
                     SetResult(ctx.Request.QueryString.Value, ctx);
                 }
-                else if (ctx.Request.Method == "POST")
-                {
-                    if (!ctx.Request.ContentType.Equals("application/x-www-form-urlencoded", StringComparison.OrdinalIgnoreCase))
-                    {
-                        ctx.Response.StatusCode = 415;
-                    }
-                    else
-                    {
-                        using (var sr = new StreamReader(ctx.Request.Body, Encoding.UTF8))
-                        {
-                            var body = await sr.ReadToEndAsync();
-                            SetResult(body, ctx);
-                        }
-                    }
-                }
                 else
                 {
                     ctx.Response.StatusCode = 405;
@@ -165,14 +150,14 @@ namespace ConsoleClientWithBrowser
 
         private void SetResult(string value, HttpContext ctx)
         {
+            _source.TrySetResult(value);
+            
             try
             {
                 ctx.Response.StatusCode = 200;
                 ctx.Response.ContentType = "text/html";
                 ctx.Response.WriteAsync("<h1>You can now return to the application.</h1>");
                 ctx.Response.Body.Flush();
-
-                _source.TrySetResult(value);
             }
             catch
             {
