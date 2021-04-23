48eb2a1a9887f0d0f8f54a917bc13c6ebee55914
Rémi Ollagnier <remi.ollagnier@gfi.fr>
2021-04-23 15:01:36 +0200

    Added the possibility to configure the length of the nonce and the state Added the raw response if HttpStatusCode is not OK (redeem code for tokens)

--- src/OidcClient/AuthorizeClient.cs
+++ src/OidcClient/AuthorizeClient.cs
@@ -93,8 +93,8 @@ namespace IdentityModel.OidcClient
 
             var state = new AuthorizeState
             {
-                Nonce = _crypto.CreateNonce(),
-                State = _crypto.CreateState(),
+                Nonce = _crypto.CreateNonce(_options.NonceLength),
+                State = _crypto.CreateState(_options.StateLength),
                 RedirectUri = _options.RedirectUri,
                 CodeVerifier = pkce.CodeVerifier,
             };
